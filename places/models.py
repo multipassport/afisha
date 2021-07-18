6bdc9aca76e293cc394ce05027f9c793c4b75879
multipassport <mashross@gmail.com>
2021-07-18 14:34:11 +0300

    Create superuser to show places on admin page

--- places/models.py
+++ places/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
-    description_short = models.CharField(
-        max_length=200, verbose_name='Короткое описание'
-    )
+    description_short = models.TextField(verbose_name='Короткое описание')
     description_long = models.TextField(verbose_name='Описание')
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
+
+    def __str__(self):
+        return self.title
