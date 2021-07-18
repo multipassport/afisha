1de7659d4b9148b49fc4897f099bc3c1d441d3d1
multipassport <mashross@gmail.com>
2021-07-18 15:08:21 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,15 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(verbose_name='Картинка')
+    place = models.ForeignKey(
+        'Place',
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
+
+    def __str__(self):
+        return f'{self.id} {self.place.title}'
