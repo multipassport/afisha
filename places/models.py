a56326a6848abb12a4684c26823604c190d52331
multipassport <mashross@gmail.com>
2021-07-18 14:15:28 +0300

    Add place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description_short = models.CharField(
+        max_length=200, verbose_name='Короткое описание'
+    )
+    description_long = models.TextField(verbose_name='Описание')
+    longitude = models.FloatField(verbose_name='Долгота')
+    latitude = models.FloatField(verbose_name='Широта')
