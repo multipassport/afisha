97378abfa74078cd8f2dacbfec5f892c873b84dd
multipassport <mashross@gmail.com>
2021-07-21 22:43:47 +0300

    Show preview of added photos

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,6 @@
 from adminsortable2.admin import SortableInlineAdminMixin
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
@@ -11,9 +12,15 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     extra = 1
     sortable_by = ('id',)
-    # raw_id_fields = ('id',)
-    # fields = ('id',)
-    # raw_id_fields = ('place',)
+    readonly_fields = ('show_image',)
+
+    def show_image(self, obj):
+        max_height = 200
+        return format_html('<img src="{url}" width="{width}" height="{height}" />'.format(
+            url=obj.image.url,
+            width=obj.image.width / (obj.image.height / max_height),
+            height=max_height
+        ))
 
 
 @admin.register(Place)
