20cd3f7157baed467efcee0afb7126a14735b4ed
multipassport <mashross@gmail.com>
2021-07-21 23:04:05 +0300

    Install tinymce text editor to edit long descriptions

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from places import views
 
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_index_page),
     path('places/<int:place_id>', views.show_place, name='places'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
