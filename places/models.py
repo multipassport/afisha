7ae32056d63f33ea1bc420df89275119e229f231
multipassport <mashross@gmail.com>
2021-07-23 13:15:48 +0300

    Set ImageField download destination

--- places/models.py
+++ places/models.py
@@ -4,8 +4,9 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
-    description_short = models.TextField(verbose_name='Короткое описание')
-    description_long = HTMLField(verbose_name='Описание')
+    description_short = models.TextField(
+        verbose_name='Короткое описание', blank=True)
+    description_long = HTMLField(verbose_name='Описание', blank=True)
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
 
@@ -14,17 +15,17 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    image = models.ImageField(verbose_name='Картинка')
+    image = models.ImageField(verbose_name='Картинка', upload_to='media/')
     position = models.PositiveSmallIntegerField(
         verbose_name='Позиция',
         default=0,
         blank=False,
-        null=False
+        null=False,
     )
     place = models.ForeignKey(
         'Place',
         on_delete=models.CASCADE,
-        related_name='images'
+        related_name='images',
     )
 
     class Meta(object):
