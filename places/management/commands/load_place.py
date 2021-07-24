ca1978a4955f0fc3a56bf7c0beb39d18171b98a4
multipassport <mashross@gmail.com>
2021-07-24 17:08:24 +0300

    Make command to create place model from json file contents

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,60 @@
+import os
+import requests
+
+from requests.exceptions import HTTPError
+from urllib.parse import urlsplit
+from django.core.files.base import ContentFile
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from places.models import Image, Place
+
+
+def download_image(url, place_id):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_content = ContentFile(response.content)
+        image_name = os.path.split(urlsplit(url).path)[-1]
+        if image_name not in os.listdir(settings.MEDIA_ROOT):
+            image, _ = Image.objects.get_or_create(
+                image=image_name, place_id=place_id)
+            image.image.save(image_name, image_content, save=True)
+    except ConnectionError:
+        raise CommandError('ConnectionError. Try again later')
+    except HTTPError:
+        raise CommandError(f'Photo from {url} is not found')
+
+
+def get_place(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except ConnectionError:
+        raise CommandError('ConnectionError. Try again later')
+    except HTTPError:
+        raise CommandError(f'JSON file from {url} is not found')
+    return response.json()
+
+
+def create_place(url):
+    place_content = get_place(url)
+    place, _ = Place.objects.get_or_create(
+        title=place_content['title'],
+        longitude=place_content['coordinates']['lng'],
+        latitude=place_content['coordinates']['lat'],
+        description_long=place_content['description_long'],
+        description_short=place_content['description_short'],
+    )
+    place_id = place.id
+    for photo_url in place_content['imgs']:
+        download_image(photo_url, place_id)
+
+
+class Command(BaseCommand):
+    help = 'Upload images from .json file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('place_url', type=str)
+
+    def handle(self, *args, **options):
+        create_place(options['place_url'])
